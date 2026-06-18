2725ff366e29fe6f5f6d776f8502b4457a4d2d57
farah-ben-harb <farah-ben-harb@users.noreply.github.com>
2026-06-19 00:14:27 +0100

    Improve Alertmanager email alerts

--- infra/terraform/azure/locals.tf
+++ infra/terraform/azure/locals.tf
@@ -5,6 +5,7 @@ locals {
   prometheus_config      = file("${path.module}/monitoring/prometheus/prometheus.yml")
   prometheus_rules       = file("${path.module}/monitoring/prometheus/rules.yml")
   alertmanager_config    = file("${path.module}/monitoring/alertmanager/alertmanager.yml")
+  alertmanager_template  = file("${path.module}/monitoring/alertmanager/templates/email.tmpl")
   blackbox_config        = file("${path.module}/monitoring/blackbox/blackbox.yml")
   grafana_datasource     = file("${path.module}/monitoring/grafana/provisioning/datasources/datasource.yml")
   grafana_dashboards     = file("${path.module}/monitoring/grafana/provisioning/dashboards/dashboards.yml")
@@ -18,16 +19,17 @@ locals {
   ])
 
   deploy_observability_script = templatefile("${path.module}/scripts/deploy_observability.sh.tftpl", {
-    stack_root           = local.observability_stack_root
-    compose_project_name = var.compose_project_name
-    env_file             = local.observability_env
-    docker_compose       = local.docker_compose_content
-    prometheus_config    = local.prometheus_config
-    prometheus_rules     = local.prometheus_rules
-    alertmanager_config  = local.alertmanager_config
-    blackbox_config      = local.blackbox_config
-    grafana_datasource   = local.grafana_datasource
-    grafana_dashboards   = local.grafana_dashboards
-    grafana_dashboard    = local.grafana_dashboard
+    stack_root            = local.observability_stack_root
+    compose_project_name  = var.compose_project_name
+    env_file              = local.observability_env
+    docker_compose        = local.docker_compose_content
+    prometheus_config     = local.prometheus_config
+    prometheus_rules      = local.prometheus_rules
+    alertmanager_config   = local.alertmanager_config
+    alertmanager_template = local.alertmanager_template
+    blackbox_config       = local.blackbox_config
+    grafana_datasource    = local.grafana_datasource
+    grafana_dashboards    = local.grafana_dashboards
+    grafana_dashboard     = local.grafana_dashboard
   })
 }
